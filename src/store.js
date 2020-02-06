3c6677abeb6fa574004b6138fc22a28bf6c3310a
codeinsack <codeinsack@gmail.com>
2020-02-06 20:39:47 +0300

    add isAuthenticated

--- src/store.js
+++ src/store.js
@@ -71,6 +71,9 @@ export default new Vuex.Store({
   getters: {
     user(state) {
       return state.user
-    }
+    },
+    isAuthenticated(state) {
+      return state.idToken !== null
+    },
   }
 })
