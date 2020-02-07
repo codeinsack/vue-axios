2521e7113b2670e9f49ad8fec23a328565950bea
codeinsack <codeinsack@gmail.com>
2020-02-07 11:26:13 +0300

    add pass validation

--- src/components/auth/signup.vue
+++ src/components/auth/signup.vue
@@ -22,19 +22,23 @@
           >
           <p v-if="!$v.age.minVal">You have to be at least {{ $v.age.$params.minVal.min }} years old</p>
         </div>
-        <div class="input">
+        <div class="input" :class="{invalid: $v.password.$error}">
           <label for="password">Password</label>
           <input
-                  type="password"
-                  id="password"
-                  v-model="password">
+            type="password"
+            id="password"
+            v-model="password"
+            @blur="$v.password.$touch()"
+          >
         </div>
-        <div class="input">
+        <div class="input" :class="{invalid: $v.confirmPassword.$error}">
           <label for="confirm-password">Confirm Password</label>
           <input
-                  type="password"
-                  id="confirm-password"
-                  v-model="confirmPassword">
+            type="password"
+            id="confirm-password"
+            v-model="confirmPassword"
+            @blur="$v.confirmPassword.$touch()"
+          >
         </div>
         <div class="input">
           <label for="country">Country</label>
@@ -75,7 +79,7 @@
 </template>
 
 <script>
-  import { required, email, numeric, minValue } from 'vuelidate/lib/validators'
+  import { required, email, numeric, minValue, minLength, sameAs } from 'vuelidate/lib/validators'
 
   export default {
     data () {
@@ -98,6 +102,16 @@
         required,
         numeric,
         minVal: minValue(18),
+      },
+      password: {
+        required,
+        minLen: minLength(5),
+      },
+      confirmPassword: {
+        // sameAs: sameAs('password'),
+        sameAs: sameAs(vm => {
+          return vm.password
+        }),
       },
     },
     methods: {
